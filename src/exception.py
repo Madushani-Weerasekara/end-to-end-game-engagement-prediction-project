9e535991c330983da9cd362ecc30bbbc234691d0
Madushani-Weerasekara <mmweerasekara@gmail.com>
2025-01-09 18:15:24 +0530

    Tested the exception.py and logger.py files

--- src/exception.py
+++ src/exception.py
@@ -1,4 +1,5 @@
 import sys # Any exception that is basically getting controll this sys library will automatically have that information.
+import logging
 
 def error_message_detail(error, error_detail:sys):
     _,_,exc_tb=error_detail.exc_info() # exc_info() will talk about the execution info, this will give you 3 important info, 1st two is not intreseted at all but last will give details about exc_tb
@@ -9,8 +10,15 @@ def error_message_detail(error, error_detail:sys):
 
 class CustomException(Exception):
     def __init__(self, error_message, error_detail:sys):
-        super.__init__(error_message) # Inherit exception class
+        super().__init__(error_message) # Inherit exception class
         self.error_message=error_message_detail(error_message,error_detail=error_detail)
 
     def __str__(self):
         return self.error_message
+    
+if __name__=="__main__":
+    try:
+        a=1/0
+    except Exception as e:
+        logging.info("Devide by zero error")
+        raise CustomException(e, sys)
--- src/utils.py
+++ src/utils.py
@@ -2,3 +2,4 @@
 # Which will be used in entire application
 # Read a dataset from database I can create a mongo db client here
 # If I want to save the model into the cloud I can write the code here
+

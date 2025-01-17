b9929c50c4fef5336b9e3018edc788bf519711b5
Madushani-Weerasekara <mmweerasekara@gmail.com>
2025-01-17 12:35:32 +0530

    Deployment started with FastAPI, HTML and CSS

--- app_api.py
+++ app_api.py
@@ -1,6 +1,9 @@
 import uvicorn # ASGI
-from fastapi import FastAPI # FastAPI for building the API
+from fastapi import FastAPI, Request # FastAPI(for building the API) and Request for handling web request
 from pydantic import BaseModel # Pydantic for request data validation
+from fastapi.responses import HTMLResponse # HTMLResponse to serve HTML pages
+from fastapi.staticfiles import StaticFiles # To serve static files like CSS, JS, images
+from fastapi.templating import Jinja2Templates # Jinja2 for rendering HTML templates
 import torch # PyTorch for the model
 import numpy as np
 import pandas as pd
@@ -8,19 +11,30 @@ from src.components.models import EngageModel
 from src.model_deployment import prepare_data_for_model
 import pickle
 
+
 # Initialize FastAPI app
 app = FastAPI()
 
-# Prediction output labels
-level = ['Low', 'Medium', 'High']
+# Mount the static directory for css, js, images etc.
+app.mount("/static", StaticFiles(directory="static", name="static"))
 
+# Configure the templates directory
+templates = Jinja2Templates(directory="templates")
+
+# Prediction output labels
+level = ['Low', 'Medium', 'High'] 
+
+"""
 # Path to the preprocessor object
 p_path = 'artifacts\\preprocessor.pkl'
-
+ 
+"""
 # Load the pre-trained model
 model = EngageModel(11, 3, 'artifacts\\testModel.pth')
 model.load_model()  
 
+ 
+"""
 # Define the input data schema for the prediction endpoint
 class ScoringItem(BaseModel):
     Age: int
@@ -34,14 +48,20 @@ class ScoringItem(BaseModel):
     AvgSessionDurationMinutes: int
     PlayerLevel: int
     AchievementsUnlocked: int
+    """
+ 
 
-#  root endpoint
-@app.get("/")
-def read_root():
-    return {"Message": "Game Engagement Model is running!"}
+#  root endpoint/ Home route serving the HTML page
+@app.get("/", response_class=HTMLResponse)
+def read_root(request=Request):
+    return templates.TemplateResponse("index.html", {"request": request, "title": "Game Engagement Model is running!"})
 
-@app.get("/predict")
-async def scoring_endpoint(item:ScoringItem):
+@app.get("/predict", response_class=HTMLResponse)
+async def predict_page(request:Request):
+    return templates.TemplateResponse("predict.html", {"request": request, "title": "Prediction Page"})
+
+   
+"""
     # Convert input data to dictionary
     data_point = item.dict()
     print(data_point)
@@ -55,8 +75,7 @@ async def scoring_endpoint(item:ScoringItem):
 
     # Return the predicted level as a response
     return {"Predicted Engagement Level:", level[pred.item()]}
-
-"""
+ 
 # To run the app directly
 if __name__ == "__main__":
     import uvicorn

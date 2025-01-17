a9a9fdb6263af64bed4c99f3e489a93f17120659
Madushani-Weerasekara <mmweerasekara@gmail.com>
2025-01-17 13:49:57 +0530

    Add FastAPI app with endpoints for home, prediction form, and result display with model integration

--- app_api.py
+++ app_api.py
@@ -10,6 +10,7 @@ import pandas as pd
 from src.components.models import EngageModel
 from src.model_deployment import prepare_data_for_model
 import pickle
+from fastapi import Form
 
 
 # Initialize FastAPI app
@@ -24,62 +25,59 @@ templates = Jinja2Templates(directory="templates")
 # Prediction output labels
 level = ['Low', 'Medium', 'High'] 
 
-"""
-# Path to the preprocessor object
-p_path = 'artifacts\\preprocessor.pkl'
- 
-"""
 # Load the pre-trained model
 model = EngageModel(11, 3, 'artifacts\\testModel.pth')
 model.load_model()  
 
- 
-"""
-# Define the input data schema for the prediction endpoint
-class ScoringItem(BaseModel):
-    Age: int
-    Gender: str
-    Location: str
-    GameGenre: str
-    PlayTimeHours: float
-    InGamePurchases: int
-    GameDifficulty: str
-    SessionsPerWeek: int
-    AvgSessionDurationMinutes: int
-    PlayerLevel: int
-    AchievementsUnlocked: int
-    """
- 
-
-#  root endpoint/ Home route serving the HTML page
+#  root endpoint -> Home Page
 @app.get("/", response_class=HTMLResponse)
 def read_root(request=Request):
     return templates.TemplateResponse("index.html", {"request": request, "title": "Game Engagement Model is running!"})
 
+# predict endpoint -> Prediction Form
 @app.get("/predict", response_class=HTMLResponse)
 async def predict_page(request:Request):
     return templates.TemplateResponse("predict.html", {"request": request, "title": "Prediction Page"})
 
-   
-"""
-    # Convert input data to dictionary
-    data_point = item.dict()
-    print(data_point)
+# Result endpoint -> Process data and show the result
+@app.post("/result", response_class=HTMLResponse)
+async def get_prediction(
+    request : Request,
+    Age: int = Form(...),
+    Gender : str = Form(...),
+    Location: str = Form(...),
+    GameGenre: str = Form(...),
+    PlayTimeHours: float = Form(...),
+    InGamePurchases: int = Form(...),
+    GameDifficulty: str = Form(...),
+    SessionsPerWeek: int = Form(...),
+    AvgSessionDurationMinutes: int = Form(...),
+    PlayerLevel: int = Form(...),
+    AchievementsUnlocked: int = Form(...)     
 
-    # Preprocess the data for the model
-    x_tensor = prepare_data_for_model(data_point, p_path)
+):
+    # Collect form data into a dictionary
+    user_data = {
+        "Age": Age,
+        "Gender": Gender,
+        "Location": Location,
+        "GameGenre": GameGenre,
+        "PlayTimeHours": PlayTimeHours,
+        "InGamePurchases": InGamePurchases,
+        "GameDifficulty": GameDifficulty,
+        "SessionsPerWeek": SessionsPerWeek,
+        "AvgSessionDurationMinutes": AvgSessionDurationMinutes,
+        "PlayerLevel": PlayerLevel,
+        "AchievementsUnlocked": AchievementsUnlocked
+    }
+   
+    preprocessed_data = prepare_data_for_model(user_data, 'artifacts\\preprocessor.pkl')
 
     # Make a prediction
-    pred = model.predict(x_tensor)
-    print(pred.item())
+    prediction = model.predict(preprocessed_data)
+    predicted_level = level[prediction.item()]
 
-    # Return the predicted level as a response
-    return {"Predicted Engagement Level:", level[pred.item()]}
- 
-# To run the app directly
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run("app_api:app", host="127.0.0.1", port=8000, reload=True)
-"""
+    # Return result page with a prediction
+    return templates.TemplateResponse("result.hrml", {"request": request, "result": predicted_level})
  
 
